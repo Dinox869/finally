8202059d83160f190ad9cb7a7b3ed813a89e58dc
dennis <jamesdennis869@gmail.com>
2020-03-17 14:26:45 +0300

    fit

--- roamemalify/src/main/java/com/roamtech/roamemalify/sop.java
+++ roamemalify/src/main/java/com/roamtech/roamemalify/sop.java
@@ -1,6 +1,18 @@
 package com.roamtech.roamemalify;
 
-public class sop {
+import android.os.Bundle;
+import android.widget.Toast;
 
-    
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sop extends AppCompatActivity {
+
+private  void OnCreate(@Nullable Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    Toast.makeText(getApplicationContext(),"This chat is Disabled.",Toast.LENGTH_SHORT).show();
+
+}
+
 }
