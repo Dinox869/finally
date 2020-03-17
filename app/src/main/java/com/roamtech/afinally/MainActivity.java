d27304ca400734dfb1bac6f2990092e87a03bcf0
dennis <jamesdennis869@gmail.com>
2020-03-17 14:27:19 +0300

    fit

--- app/src/main/java/com/roamtech/afinally/MainActivity.java
+++ app/src/main/java/com/roamtech/afinally/MainActivity.java
@@ -1,8 +1,9 @@
 package com.roamtech.afinally;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"This chat is Disabled.",Toast.LENGTH_SHORT).show();
     }
 }
